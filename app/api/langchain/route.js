e852388dbbe4e49cf03a5837913df96fdf3137b3
trumpetdoot <andyyin06@gmail.com>
2023-10-15 01:37:41 -0400

    build duel page

--- app/api/langchain/route.js
+++ app/api/langchain/route.js
@@ -1,7 +1,6 @@
 import { LLMChain } from "langchain/chains";
 import { OpenAI } from "langchain/llms/openai";
 import { PromptTemplate } from "langchain/prompts";
-import { disconnect } from "mongoose";
 
 const llm = new OpenAI({
   openAIApiKey: "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", 
@@ -11,12 +10,10 @@ const prompt = new PromptTemplate({
   inputVariables: ["code"],
   template: "Please rate this code '{code}' out of 10 in terms of quality. Example Answer: 1/10. I gave this rating because..."
 })
-
 const chain = new LLMChain({
   llm: llm, 
   prompt: prompt,
 })
-
 const output = await chain.call({
   code: "const json = { key: value };",
 })
---
+++ app/duel.js
@@ -0,0 +1,49 @@
+export default function Duel() {
+    return (
+      <div className="flex">
+        {/* User side */}
+        <div className="w-1/2 h-screen bg-gray-500">
+          <div className="w-full my-auto">
+            <div className="w-full h-8 bg-gray-300 border border-gray-500 rounded">
+              <div className="h-8 bg-green-500" style={{ width: '90%' }}>
+                Health: 90%
+              </div>
+            </div>
+          </div>
+          <div contentEditable="true" className="flex items-center">
+            const firstnumber = 5; <br />
+            let totalSum = 0;  <br />
+            for(let i = 0; i &lt; firstnumber; i++)  <br />
+            &nbsp;&nbsp;&nbsp;totalSum = totalSum+1; <br />
+            console.log(The TOTALSUM of number is: " " + totalSum);
+          </div>
+          <div className="flex bottom-0 h-16 w-full">
+            <textarea className="w-full h-40 bg-gray-500 border placeholder-gray-200 text-sm" placeholder="Why did you change the things you did?">
+              {/* Cdoe input */}
+            </textarea>
+          </div>
+        </div>
+  
+        {/*  Opp side */}
+        <div className="w-1/2 h-screen bg-white-500">
+          <div className="w-full my-auto">
+            <div className="w-full h-8 bg-gray-300 border border-gray-500 rounded">
+              <div className="h-8 bg-red-500" style={{ width: '80%' }}>
+                Health: 80%
+              </div>
+            </div>
+          </div>
+          <div className="flex items-center">
+            <pre>
+              const firstnumber = 5; <br />
+              let totalSum = 0; <br />
+              for(let i = 0; i &lg; firstnumber; i++) <br />
+              &nbsp;&nbsp;&nbsp;totalSum = totalSum+1; <br />
+              console.log(The TOTALSUM of number is: " " + totalSum);
+            </pre>
+          </div>
+        </div>
+      </div>
+    )
+  }
+  
